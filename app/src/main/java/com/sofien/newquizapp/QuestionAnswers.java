8c43be459e5afeaec450577b3522ac5e564aa3b1
SofTheDev <sofienbenktib11@gmail.com>
2022-11-04 11:58:01 +0100

    new questions added

--- app/src/main/java/com/sofien/newquizapp/QuestionAnswers.java
+++ app/src/main/java/com/sofien/newquizapp/QuestionAnswers.java
@@ -2,18 +2,39 @@ package com.sofien.newquizapp;
 
 public class QuestionAnswers {
     public static String question[]={
-            "Which company owns Android ?",
-            "Which one is not a programming language ?",
-            "Which framework is used to develop Android apps ?"
+            "Which team won the UCL in 2021/2022 season ?",
+            "Which country will host the 2022 World Cup ?",
+            "Who was the ballon d'or winner in 2022 ?",
+            "Who was the player to win both UCL and WC in 2018 ?",
+            "Which team won 2 Europa League titles back to back ?",
+            "Which player has more ballon d'or ?",
+            "In which football club does Zlatan Ibrahimovic plays currently ?",
+            "Which club does Antonio Conte Manage currently ?",
+            "Which football coach won 3 back-to-back UCL titles ?",
+            "Which football club will never win a UCL title ?"
     };
     public static String choices[][]={
-            {"Google","Apple","Nokia","Samsung"},
-            {"HTMl","Dart","Java","Kotlin"},
-            {"Angular","Android Studio","DevOps","VueJs"}
+            {"Real Madrid","FC Bayern","Chelsea FC","Man City"},
+            {"India","Qatar","France","Canada"},
+            {"Neymar","Karim Benzema","Sadio Mané","Mohamad Salah"},
+            {"Kylian Mbappé","Raphael Varane","Luka Modric","Ivan Perisic"},
+            {"Bayer Liverkuzen","FC Sevilla","Inter Milan","Borussia Dortmund"},
+            {"David Beckham","Leonel Messi","Braithwaite","Jordi Alba"},
+            {"AC Milan","Liverpool FC","Arsenal","Inter Milan"},
+            {"Westham united","Tottenham","Villareal","FC Miami"},
+            {"Jupp Heinks","Zidane","Sir Alex Firguson","Arsene Wenger"},
+            {"Paris Saint Germain","Nottingam Forest","Osasuna","FC Porto"}
     };
     public static String correctAnswer[]={
-            "Google",
-            "HTML",
-            "Android Studio"
+            "Real Madrid",
+            "Qatar",
+            "Karim Benzema",
+            "Rapahel Varane",
+            "FC Sevilla",
+            "Leonel Messi",
+            "AC Milan",
+            "Tottenham",
+            "Zidane",
+            "Paris Saint Germain"
     };
 }
